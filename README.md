afa9a6001683a83b22c5368cd09a434735fdabbe
Jean Machuca <correojean@gmail.com>
2023-04-29 00:36:01 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# @types/qcobjects-api
+
+This packages contains common types used with QCObjects Framework and QCObjects Microservices API
+Learn more about QCObjects in the [QCObjects official website](https://qcobjects.dev)
---
+++ index.d.ts
@@ -0,0 +1,52 @@
+// Type definitions for QCObjects 2.4
+// Project: https://qcobjects.dev
+// Definitions by: Jean Machuca <https://github.com/jeanmachuca>
+// Definitions: https://qcobjects.dev
+
+import { ClientRequest } from "http";
+import { Http2SecureServer, Http2Server, Http2ServerRequest, Http2Stream } from "http2";
+import { InheritClass } from "qcobjects";
+import { Stream } from "stream";
+
+/**
+ * Types for TypeScript support in QCObjects Microservices
+ */
+
+declare namespace api {
+    class Route {
+        name:string;
+        description:string;
+        microservice:string;
+        redirect_to?:string;
+        responseHeaders:any;
+        cors:any;
+    }
+
+    class BackendMicroservice extends InheritClass{
+        domain:string;
+        basePath:string;
+        route:Route;
+        projectPath?:string;
+        stream?:Http2Stream | Stream;
+        request?:Http2ServerRequest | ClientRequest;
+        routeParams?:any;
+        server: Http2SecureServer | Http2Server;
+
+        cors():void;
+        head(formData?:object|string):void;
+        get(formData?:object|string):void;
+        post(formData?:object|string):void;
+        put(formData?:object|string):void;
+        delete(formData?:object|string):void;
+        connect(formData?:object|string):void;
+        options(formData?:object|string):void;
+        trace(formData?:object|string):void;
+        patch(formData?:object|string):void;
+        finishWithBody(stream?:Http2Stream | Stream):void;
+        done():void;
+
+    }
+
+}
+
+export = api;
